7d8c354e37aa9fc81f2f65063072d7ac26c3593e
Phil Plückthun <phil@piemapping.com>
2016-04-28 14:48:36 +0100

    Remove Popup for now

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import ReactMapboxGl from "./map";
 
 import Layer from "./layer";
 import Feature from "./feature";
-import Popup from "./popup";
 
 export { ReactMapboxGl };
 export { Layer };
 export { Feature };
-// export { Popup };
 
 export default ReactMapboxGl;
 
