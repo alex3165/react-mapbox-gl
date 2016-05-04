7dd91ff5aefd53ccfd9a63904eed92aa57f0ec44
Phil Plückthun <phil@plckthn.me>
2016-05-04 22:38:29 +0100

    Change Map export to just be named "Map"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import ReactMapboxGl from "./map";
-
+import Map from "./map";
 import Layer from "./layer";
 import Feature from "./feature";
 import ZoomControl from "./zoom-control";
 import Popup from "./popup";
 
-export { ReactMapboxGl };
-export { Layer };
-export { Feature };
-export { ZoomControl };
-export { Popup };
+export {
+  Feature,
+  Layer,
+  Map,
+  Popup,
+  ZoomControl
+};
 
-export default ReactMapboxGl;
+export default Map;
 
