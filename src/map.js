dea509ce31309cf844facbb1087dd7eaa2d9eae3
Alexandre Rieux <alexr.3165@gmail.com>
2016-05-31 22:17:51 +0100

    First attempt to build mapbox-gl-js with webpack

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import MapboxGl from "mapbox-gl/dist/mapbox-gl";
+import MapboxGl from "./mapbox-gl-js.bundle";
 import React, { Component, PropTypes } from "react";
 import isEqual from "deep-equal";
 
