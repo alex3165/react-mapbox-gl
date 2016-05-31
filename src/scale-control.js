3b555587158e14722c8086385d95394017f63746
Arthur <arthur.parmentier@hec.edu>
2016-05-31 16:12:12 +0100

    Added UI

--- src/scale-control.js
+++ src/scale-control.js
@@ -1,35 +1,50 @@
 import React, { Component } from "react";
 
-// in meters
 const scales = [
+  .01, .02, .05,
+  .1, .2, .5,
+  1, 2, 5,
   10, 20, 50,
   100, 200, 500,
   1000, 2 * 1000, 5 * 1000,
-  10 * 1000, 20 * 1000, 50 * 1000,
-  100 * 1000, 200 * 1000, 500 * 1000,
-  1000 * 1000, 2 * 1000 * 1000, 5 * 1000 * 1000,
-  10 * 1000 * 1000
+  10 * 1000
 ];
 
 const containerStyle = {
   position: "absolute",
   zIndex: 2,
-  display: "flex",
-  flexDirection: "column",
   boxShadow: "0px 1px 4px rgba(0, 0, 0, .3)",
   border: "1px solid rgba(0, 0, 0, 0.1)",
   bottom: 10,
-  right: 100
+  right: 50,
+  backgroundColor: "#fff",
+  opacity: .85,
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "baseline",
+  padding: "3px 7px"
+};
+
+const scaleStyle = {
+  border: "2px solid #7e8490",
+  borderTop: "none",
+  height: 7,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0
 };
 
 const MEASUREMENTS = [ "km", "mi" ];
 
+const MILE_IN_KILOMETERS = 1.60934;
+const MILE_IN_FEET = 5280;
+const KILOMETER_IN_METERS = 1000;
+
 const MIN_WIDTH_SCALE = 40;
 
 export default class ScaleControl extends Component {
-  state = {
-    chosen_scale: false
-  }
+   static contextTypes = {
+    map: React.PropTypes.object
+  };
 
   static propTypes = {
     measurement: React.PropTypes.string
@@ -39,28 +54,52 @@ export default class ScaleControl extends Component {
     measurement: MEASUREMENTS[0]
   };
 
-  static contextTypes = {
-    map: React.PropTypes.object
+  state = {
+    chosenScale: false,
+    scaleWidth: MIN_WIDTH_SCALE
   };
 
   componentWillMount() {
     const { map } = this.context;
+    this._setScale(map);
+
+    map.on("zoomend", (...args) => {
+      this._setScale(map);
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.state.map) {
+      this.state.map.off();
+    }
+  }
+
+  _setScale = (map) => {
+    const { measurement } = this.props;
     const clientWidth = map._canvas.canvas.clientWidth;
     const { _ne, _sw } = map.getBounds();
 
-    const totalWidth = 1000 * this._getDistanceTwoPoints([ _sw.lng, _ne.lat ], [ _ne.lng, _ne.lat ]);
+    const totalWidth = this._getDistanceTwoPoints([ _sw.lng, _ne.lat ], [ _ne.lng, _ne.lat ], measurement);
     const relativeWidth = totalWidth / clientWidth * MIN_WIDTH_SCALE;
 
-    const chosen_scale = scales.reduce((acc, curr) => acc || (curr > relativeWidth && curr), 0);
-    this.setState({ chosen_scale });
-    console.log("chosen_scale", chosen_scale);
-  }
+    const chosenScale = scales.reduce((acc, curr) => acc || (curr > relativeWidth && curr), 0);
+    const canvasWidth = map._canvas.canvas.width;
+    const scaleWidth = chosenScale / totalWidth * map._canvas.canvas.width;
 
-  _getDistanceTwoPoints(x, y) {
+    this.setState({
+      chosenScale,
+      scaleWidth
+    });
+  };
+
+  _getDistanceTwoPoints(x, y, measurement = "km") {
     const [ lng1, lat1 ] = x;
     const [ lng2, lat2 ] = y;
 
-    const R = 6371; // Radius of the earth in km
+    // Radius of the earth in km or miles
+    const R = measurement === "km"
+      ? 6371
+      : 6371 / MILE_IN_KILOMETERS;
     const dLat = this._deg2rad(lat2 - lat1);
     const dLng = this._deg2rad(lng2 - lng1);
     const a =
@@ -68,7 +107,7 @@ export default class ScaleControl extends Component {
       Math.cos(this._deg2rad(lat1)) * Math.cos(this._deg2rad(lat2)) *
       Math.sin(dLng/2) * Math.sin(dLng/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    const d = R * c; // Distance in km
+    const d = R * c;
 
     return d;
   }
@@ -77,12 +116,35 @@ export default class ScaleControl extends Component {
     return deg * (Math.PI/180);
   }
 
+  _displayMeasurement(measurement, chosenScale) {
+    if (chosenScale >= 1) {
+      return `${chosenScale} ${measurement}`;
+    }
+
+    if (measurement === "mi") {
+      return `${Math.floor(chosenScale * MILE_IN_FEET)} ft`;
+    }
+
+    return `${Math.floor(chosenScale * KILOMETER_IN_METERS)} m`;
+  }
+
   render() {
     const { map } = this.context;
+    const { measurement } = this.props;
+    const { chosenScale, scaleWidth } = this.state;
 
     return (
       <div style={containerStyle}>
-        Scale
+        <div
+          style={{
+            ...scaleStyle,
+            width: scaleWidth
+          }}>
+        </div>
+
+        <div style={{ paddingLeft: 10 }}>
+          {this._displayMeasurement(measurement, chosenScale)}
+        </div>
       </div>
     );
   }
