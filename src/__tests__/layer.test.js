f5f1e19987a78c22791649c8ef8c9d5ecd8b9981
Alexandre Rieux <alexr.3165@gmail.com>
2016-11-21 17:56:29 +0000

    Add basic jest setup

--- src/__tests__/layer.test.js
+++ src/__tests__/layer.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import Layer from './layer';
+import Layer from '../layer';
 import renderer from 'react-test-renderer';
+import { withContext } from 'recompose';
 
 test('Layer', () => {
-  const LayerComponent = renderer.create(
-    <Layer/>
-  );
+  describe('Should test Layer', () => {
+    let LayerWithContext;
+
+    beforeAll(() => {
+      LayerWithContext = withContext({
+        map: React.PropTypes.object
+      }, () => ({
+        map: {
+          addSource: () => null,
+          addLayer: () => null,
+          on: () => null
+        }
+      }))(Layer);
+    });
+
+    it('Should render layer', () => {
+      const LayerComponent = renderer.create(
+        <LayerWithContext/>
+      );
+    });
+  });
 });
