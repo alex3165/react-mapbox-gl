cc59d1ca22a82e25272dfc2e8b606e20f66311e6
Alexandre Rieux <alexr.3165@gmail.com>
2016-11-22 09:23:19 +0000

    Add very trivial tests

--- src/__tests__/layer.test.js
+++ src/__tests__/layer.test.js
@@ -5,28 +5,53 @@ import { withContext } from 'recompose';
 
 describe('Layer', () => {
   let LayerWithContext;
+  let addLayerMock;
+  let addSourceMock;
 
-  beforeAll(() => {
+  beforeEach(() => {
+    addLayerMock = jest.fn();
+    addSourceMock = jest.fn();
+
     LayerWithContext = withContext({
       map: React.PropTypes.object
     }, () => ({
       map: {
-        addSource: jest.fn(),
-        addLayer: jest.fn(),
+        addSource: addSourceMock,
+        addLayer: addLayerMock,
         on: jest.fn(),
         getSource: jest.fn().mockReturnValue({ setData: jest.fn() })
       }
     }))(Layer);
   });
 
-  it('Should render layer', () => {
+  it('Should render layer with default options', () => {
     const LayerComponent = TestUtils.renderIntoDocument(
       <LayerWithContext
         children={[{ props: {}}]}/>
     );
 
-    // console.log(LayerComponent);
-
-    // expect(LayerComponent).toMatchSnapshot();
+    expect(addLayerMock.mock.calls[0]).toEqual([{
+      id: 'layer-1',
+      source: 'layer-1',
+      type: 'symbol',
+      layout: {},
+      paint: {}
+    }, undefined]);
   });
+
+  it('Should render layer with default source', () => {
+    const LayerComponent = TestUtils.renderIntoDocument(
+      <LayerWithContext
+        children={[{ props: {}}]}/>
+    );
+
+    expect(addSourceMock.mock.calls[0]).toEqual(['layer-2', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [],
+      }
+    }]);
+  });
+
 });
