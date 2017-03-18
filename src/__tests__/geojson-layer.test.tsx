07d33783cf882452c5e9e0e5a73d2de9b2ce2be8
Jared Luxenberg <jared@jaredlux.com>
2017-03-17 17:33:15 -0700

    GeoJSONLayer accepts layerOptions
    layerOptions was missing from GeoJSONLayer, this
    adds it. I've also added a test.

---
+++ src/__tests__/geojson-layer.test.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import GeoJSONLayer from '../geojson-layer';
+import { withContext } from 'recompose';
+import { mount } from 'enzyme';
+
+describe('GeoJSONLayer', () => {
+  let GeoJSONLayerWithContext: any;
+  let addLayerMock = jest.fn();
+
+  beforeEach(() => {
+    addLayerMock = jest.fn();
+
+    GeoJSONLayerWithContext = withContext({
+      map: React.PropTypes.object
+    }, () => ({
+      map: {
+        addSource: jest.fn(),
+        addLayer: addLayerMock
+      }
+    }))(GeoJSONLayer);
+  });
+
+  it('Should call addLayer with provided layerOptions', () => {
+    mount(
+      <GeoJSONLayerWithContext
+        fillPaint={{'fill-color': '#001122'}}
+        data={{ "type": "FeatureCollection", "features": []}}
+        layerOptions={{minzoom: 13}}
+      /> as React.ReactElement<any>
+    );
+
+    const addFillLayerCall = addLayerMock.mock.calls.find(([ call ]) => call.id.endsWith('-fill'));
+    expect(addFillLayerCall).toEqual([{
+      id: 'geojson-1-fill',
+      source: 'geojson-1',
+      type: 'fill',
+      layout: {visibility: 'visible'},
+      minzoom: 13,
+      paint: {'fill-color': '#001122'}
+    }, undefined]);
+  });
+
+  it('Should call addLayer when no layerOptions provided', () => {
+    mount(
+      <GeoJSONLayerWithContext
+        fillPaint={{'fill-color': '#001122'}}
+        data={{ "type": "FeatureCollection", "features": []}}
+      /> as React.ReactElement<any>
+    );
+
+    const addFillLayerCall = addLayerMock.mock.calls.find(([ call ]) => call.id.endsWith('-fill'));
+    expect(addFillLayerCall).toEqual([{
+      id: 'geojson-2-fill',
+      source: 'geojson-2',
+      type: 'fill',
+      layout: {visibility: 'visible'},
+      paint: {'fill-color': '#001122'}
+    }, undefined]);
+  });
+});
