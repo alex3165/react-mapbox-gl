bb2ed97e2b28bb289ea84a8fac7a32cd71a07cf8
stereobooster <stereobooster@gmail.com>
2017-05-03 00:19:26 +0200

    Simplified Attribution

--- example/src/components/Attribution.js
+++ example/src/components/Attribution.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Attribution extends Component {
-  render() {
-    return (
+export default function Attribution(props) {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        right: 0,
+        bottom: 0,
+        width: props.width,
+        height: props.height,
+        pointerEvents: 'none',
+        zIndex: 10
+      }}
+    >
       <div
         style={{
           position: 'absolute',
           right: 0,
           bottom: 0,
-          width: this.props.width,
-          height: this.props.height,
-          pointerEvents: 'none',
-          zIndex: 10
+          lineHeight: 'initial',
+          background: 'rgba(255,255,255,0.5)',
+          padding: 5,
+          pointerEvents: 'all'
         }}
       >
-        <div
-          style={{
-            position: 'absolute',
-            right: 0,
-            bottom: 0,
-            lineHeight: 'initial',
-            background: 'rgba(255,255,255,0.5)',
-            padding: 5,
-            pointerEvents: 'all'
-          }}
-        >
-          { this.props.children }
-        </div>
+        { props.children }
       </div>
-    )
-  }
+    </div>
+  )
 }
