b3d89cfc48ff85de2012f125ffd6ea54718307c3
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-11 10:35:46 +0100

    Add html cluster back

--- example/src/demos/index.tsx
+++ example/src/demos/index.tsx
@@ -3,6 +3,8 @@ import LondonCycleMap from './londonCycle';
 import AllShapes from './allShapes';
 import HtmlFeatures from './htmlFeatures';
 import ThreeDMap from './threeDMap';
+import HtmlCluster from './htmlCluster';
+
 import styled from 'styled-components';
 
 const Title = styled.h1`
@@ -22,6 +24,8 @@ class Demos extends React.Component<{}, {}> {
         <HtmlFeatures/>
         <Title>3D extrusion mapbox map</Title>
         <ThreeDMap/>
+        <Title>Html cluster features</Title>
+        <HtmlCluster/>
       </div>
     );
   }
