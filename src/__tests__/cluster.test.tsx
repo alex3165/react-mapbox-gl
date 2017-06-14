97185c996117ec9d9dbdbd94860942c5917aaedd
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-14 13:48:01 +0100

    wip test

--- src/__tests__/cluster.test.tsx
+++ src/__tests__/cluster.test.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 import Cluster from '../cluster';
+import Marker from '../marker';
 import { withContext } from 'recompose';
 import { shallow } from 'enzyme';
+
+const coordinates = [
+  [-12.408741828510017, 58.16339752811908],
+  [-5.668629523822517, 50.06970856327533],
+  [-9.26996282865152, 42.92873605781255],
+  [-8.969410773822517, 37.03827545780658],
+  [11.024180534771233, 37.07398102421283],
+  [-9.273915168353767, 32.58161041874408]
+]
 
 describe('cluster', () => {
   let ClusterWithContext: any;
@@ -23,19 +33,22 @@ describe('cluster', () => {
           }))
         }
       })
-    )(Layer);
+    )(Cluster);
   });
 
   it('should render the correct number of cluster', () => {
-    // const clusterMarkerFactory = jest.fn(() => {
-
-    // });
+    const clusterMarkerFactory = jest.fn();
 
     shallow(
-      <Cluster
-        children={}
-        ClusterMarkerFactory={}
+      <ClusterWithContext
+        children={
+          coordinates.map(coord => (
+            <Marker coordinates={coord}/>
+          ))
+        }
+        ClusterMarkerFactory={clusterMarkerFactory}
       />
-    )
+    );
+
   });
 });
