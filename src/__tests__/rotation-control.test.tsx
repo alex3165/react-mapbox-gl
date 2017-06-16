9d96a13ca0f74bd2e05c9e5a6bd5560192add0e4
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-16 21:04:29 +0100

    Add controls test

---
+++ src/__tests__/rotation-control.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import RotationControl from '../rotation-control';
+import { shallow } from 'enzyme';
+
+describe('RotationControl', () => {
+  it('should render the component', () => {
+    const wrapper = shallow(
+      <RotationControl/>
+    );
+
+    expect(wrapper).toBeDefined();
+  });
+});
---
+++ src/__tests__/scale-control.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import ScaleControl from '../scale-control';
+import { shallow } from 'enzyme';
+import { withContext } from 'recompose';
+
+describe('ScaleControl', () => {
+  let ScaleControlWithContext: any;
+
+  beforeEach(() => {
+    ScaleControlWithContext = withContext(
+      {
+        map: React.PropTypes.object
+      },
+      () => ({
+        map: {
+          getBounds: {
+            _ne: { lng: 0, lat: 0 },
+            _sw: { lng: 0, lat: 0 }
+          }
+        }
+      })
+    )(ScaleControl);
+  });
+
+  it('should render the component', () => {
+    const wrapper = shallow(
+      <ScaleControlWithContext/>
+    );
+
+    expect(wrapper).toBeDefined();
+  });
+});
