a95adb855492921ebaa7cfcb239eacf2c5674274
Alexandre Rieux <alexr.3165@gmail.com>
2017-09-03 12:16:56 +0100

    Fix feature type

--- src/feature.ts
+++ src/feature.ts
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 export interface Props {
   // lat lng array (circle, line, symbol) or array of array... of lat lng (polygons)
-  coordinates: number[] | number[][][] | number[][][][];
+  coordinates: number[] | number[][] | number[][][] | number[][][][];
   properties?: any;
   onClick?: React.MouseEventHandler<HTMLElement>;
   onMouseEnter?: React.MouseEventHandler<HTMLElement>;
