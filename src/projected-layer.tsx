92ec5bee6bbde3ceb17e9c80d6f9d894cca328c2
Braden M. Kelley <redbmk@gmail.com>
2018-01-22 11:59:15 -0800

    Allow scroll events on HTML layers
    Fixes #217
    Uses ReactDOM.createPortal, which requires an update to React 16

--- src/projected-layer.tsx
+++ src/projected-layer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { createPortal } from 'react-dom';
 import * as PropTypes from 'prop-types';
 import { Map, Point } from 'mapbox-gl';
 import { OverlayParams, overlayState, overlayTransform } from './util/overlays';
@@ -93,6 +94,16 @@ export default class ProjectedLayer extends React.Component<
     map.off('move', this.handleMapMove);
   }
 
+  public renderIntoContainer(child: React.ReactNode): React.ReactNode {
+    if (this.props.type === 'marker') {
+      const container = this.context.map.getCanvasContainer();
+
+      return container ? createPortal(child, container) : child;
+    }
+
+    return child;
+  }
+
   public render() {
     const {
       style,
@@ -115,7 +126,7 @@ export default class ProjectedLayer extends React.Component<
     };
     const anchorClassname =
       anchor && type === 'popup' ? `mapboxgl-popup-anchor-${anchor}` : '';
-    return (
+    return this.renderIntoContainer(
       <div
         className={`${className} ${anchorClassname}`}
         onClick={onClick}
