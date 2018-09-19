fe1f9ff7b2670ff4eb98ad0b74f7d43fc1ce9e98
Marko K <mklopets@gmail.com>
2018-09-19 16:09:33 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -2842,7 +2842,7 @@ exports.default = map_1.default;
 /* 10 */
 /***/ (function(module, exports) {
 
-module.exports = {"token":"pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2psZXViZnIyMG5xdDNqbmpiejcydTE4MSJ9.Feyg1TyWWRs6nclrYcYTJA","styles":{"londonCycle":"mapbox://styles/alex3165/cj2hv9v4y00242slphcyk9oca","light":"mapbox://styles/mapbox/light-v9","dark":"mapbox://styles/mapbox/dark-v9","basic":"mapbox://styles/mapbox/basic-v9","outdoor":"mapbox://styles/mapbox/outdoors-v10"}}
+module.exports = {"token":"pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA","styles":{"londonCycle":"mapbox://styles/alex3165/cj2hv9v4y00242slphcyk9oca","light":"mapbox://styles/mapbox/light-v9","dark":"mapbox://styles/mapbox/dark-v9","basic":"mapbox://styles/mapbox/basic-v9","outdoor":"mapbox://styles/mapbox/outdoors-v10"}}
 
 /***/ }),
 /* 11 */
