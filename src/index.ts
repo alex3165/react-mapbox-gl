e740987ba962bf57785138f2ab5fce5475a38d54
olso <olsansky.martin@gmail.com>
2019-12-06 14:53:30 +0100

    chore: Export props as separate types

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 // Add a style tag to the document's head for the map's styling
-import Map from './map';
-import BaseLayer from './layer';
+import Map, { Props as MapProps } from './map';
+import { Events as MapEvents } from './map-events';
+import BaseLayer, { Props as BaseLayerProps } from './layer';
 import layerMouseTouchEvents from './layer-events-hoc';
-import GeoJSONLayer from './geojson-layer';
-import Feature from './feature';
-import ZoomControl from './zoom-control';
-import Popup from './popup';
-import ScaleControl from './scale-control';
-import Marker from './marker';
-import Source from './source';
-import Cluster from './cluster';
-import RotationControl from './rotation-control';
-import Image from './image';
+import GeoJSONLayer, { Props as GeoJSONLayerProps } from './geojson-layer';
+import Feature, { Props as FeatureProps } from './feature';
+import ZoomControl, { Props as ZoomControlProps } from './zoom-control';
+import Popup, { Props as PopupProps } from './popup';
+import ScaleControl, { Props as ScaleControlProps } from './scale-control';
+import Marker, { Props as MarkerProps } from './marker';
+import Source, { Props as SourceProps } from './source';
+import Cluster, { Props as ClusterProps } from './cluster';
+import RotationControl, {
+  Props as RotationControlProps
+} from './rotation-control';
+import Image, { Props as ImageProps } from './image';
 import { withMap, MapContext } from './context';
 
 const Layer = withMap(layerMouseTouchEvents(BaseLayer));
@@ -30,6 +33,22 @@ export {
   RotationControl,
   Image,
   MapContext
+};
+
+export {
+  MapProps,
+  MapEvents,
+  BaseLayerProps,
+  GeoJSONLayerProps,
+  FeatureProps,
+  ZoomControlProps,
+  PopupProps,
+  ScaleControlProps,
+  MarkerProps,
+  SourceProps,
+  ClusterProps,
+  RotationControlProps,
+  ImageProps
 };
 
 export default Map;
